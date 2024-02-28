8f989b8d6fcf8fde2c385e5d82994a7422e51f31
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2024-02-28 12:01:35 +0600

    removed unnecessary codes

--- app.js
+++ app.js
@@ -9,23 +9,11 @@ app.use(express.json());
 app.use(cors());
 
 const wifiConnectionRoute = require('./routes/v1/wifiConnection.route')
-
-const categoriesRoute = require('./routes/v1/jobCategories.route')
-const jobsRoute = require('./routes/v1/job.route')
-const employersRoute = require('./routes/v1/employer.route')
-const jobSeekerRoute = require('./routes/v1/jobSeeker.route')
-const applicationRoute = require('./routes/v1/application.route')
 const userRoute = require('./routes/v1/user.route')
 
 
 // routes
 app.use('/api/v1/wificonnection', wifiConnectionRoute)
-
-app.use('/api/v1/categories', categoriesRoute)
-app.use('/api/v1/jobs', jobsRoute)
-app.use('/api/v1/employers', employersRoute)
-app.use('/api/v1/jobSeeker', jobSeekerRoute)
-app.use('/api/v1/application', applicationRoute)
 app.use('/api/v1/user', userRoute)
 
 app.get("/", (req, res) => {
